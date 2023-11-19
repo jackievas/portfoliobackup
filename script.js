00643bd4f90f4ba7421aa01b2327cff3a51fd63c
jackievas <145609624+jackievas@users.noreply.github.com>
2023-11-19 15:16:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
 // script.js
 
-// Fetch and process JSON data
-fetch('production_jobs_data.json')
-  .then(response => response.json())
-  .then(data => {
-    console.log('JSON Data:', data);
+// Function to fetch and process JSON data
+function fetchAndProcessJSON() {
+    fetch('production_jobs_data.json')
+        .then(response => response.json())
+        .then(data => {
+            console.log('JSON Data:', data);
 
-    // Perform client-side manipulation with JSON data
+           
+            const jobTitles = data.production_jobs.map(job => job.title);
+            console.log('Job Titles:', jobTitles);
 
-    const jobTitles = data.production_jobs.map(job => job.job_title);
-    console.log('Job Titles:', jobTitles);
-  })
-  .catch(error => console.error('Error fetching JSON:', error));
+            // Display job titles on the webpage
+            const jobTitlesOutput = document.getElementById('jobTitles');
+            jobTitlesOutput.textContent = 'Job Titles: ' + jobTitles.join(', ');
+        })
+        .catch(error => console.error('Error fetching JSON:', error));
+}
 
-// Fetch and process XML data
-fetch('employee_data.xml')
-  .then(response => response.text())
-  .then(xmlString => {
-    const parser = new DOMParser();
-    const xmlDoc = parser.parseFromString(xmlString, 'text/xml');
-    console.log('XML Data:', xmlDoc);
+// Function to fetch and process XML data
+function fetchAndProcessXML() {
+    fetch('employee_data.xml')
+        .then(response => response.text())
+        .then(xmlString => {
+            const parser = new DOMParser();
+            const xmlDoc = parser.parseFromString(xmlString, 'text/xml');
+            console.log('XML Data:', xmlDoc);
 
-    // Perform client-side manipulation with XML data
-    // For example, extract employee names from XML
-    const employeeNameElements = xmlDoc.getElementsByTagName('name');
-    const employeeNamesFromXML = Array.from(employeeNameElements).map(nameElement => nameElement.textContent);
-    console.log('Employee Names from XML:', employeeNamesFromXML);
-  })
-  .catch(error => console.error('Error fetching XML:', error));
+         
+            const employeeNameElements = xmlDoc.getElementsByTagName('name');
+            const employeeNamesFromXML = Array.from(employeeNameElements).map(nameElement => nameElement.textContent);
+            console.log('Employee Names from XML:', employeeNamesFromXML);
+
+            // Display employee names on the webpage
+            const employeeNamesOutput = document.getElementById('employeeNames');
+            employeeNamesOutput.textContent = 'Employee Names: ' + employeeNamesFromXML.join(', ');
+        })
+        .catch(error => console.error('Error fetching XML:', error));
+}
